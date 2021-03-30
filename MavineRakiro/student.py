cc5d398227311cb87450de89be475cd9a1415158
MavineRakiro <mavinesamrakiro@gmail.com>
2021-03-30 22:09:19 +0300

    student.py

---
+++ MavineRakiro/student.py
@@ -0,0 +1,12 @@
+class student:
+
+    def __init__(self, name, major, gpa):
+        self.name = name
+        self.major = major
+        self.gpa = gpa
+
+    def on_honor_roll(self):
+        if self.gpa >= 70:
+            return True
+        else:
+            return False
